f4d97f60b2a5a3e4a49d731ef8d5814b8a581d04
Makkynm <makkynm@gmail.com>
2021-04-07 08:26:54 +0900

    minimized cash size

--- HerokuBlogMura.py
+++ HerokuBlogMura.py
@@ -13,7 +13,12 @@ def AutoBlogMura(BlogURL,SeachTerm, BlogName):
   driver_path = '/app/.chromedriver/bin/chromedriver'
   options = webdriver.ChromeOptions()
   options.add_argument('--headless')
+  options.add_argument('--disable-gpu')
+  options.add_argument('--no-sandbox')
+  options.add_argument('--disable-dev-shm-usage')
+  options.add_argument('--remote-debugging-port=9222')
   driver = webdriver.Chrome(options=options, executable_path=driver_path)
+  driver.set_window_size(950, 800)
 
   wait = WebDriverWait(driver, 10)
   driver.implicitly_wait(10) # 秒 暗示的待機
